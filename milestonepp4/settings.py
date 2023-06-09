db81a614b72bcf73d7ec640c4a030af0ee2c1bf5
T-KibCode <tkibirige94@gmail.com>
2023-06-09 11:24:13 +0000

    added allauth templates/ base templates

--- milestonepp4/settings.py
+++ milestonepp4/settings.py
@@ -72,13 +72,11 @@ TEMPLATES = [
 ]
 
 AUTHENTICATION_BACKENDS = [
-    
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
 
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
-    
 ]
 
 SITE_ID = 1
