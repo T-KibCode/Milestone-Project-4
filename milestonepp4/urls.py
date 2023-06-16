b501f077a7916d37c27e6ee905aa05d0e1909733
T-KibCode <tkibirige94@gmail.com>
2023-06-16 09:50:26 +0000

    adding fonts to file

--- milestonepp4/urls.py
+++ milestonepp4/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.url.statis import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls'))
-]
+] + static(settings.MEDUA_URL, document_root=settings.MEDIA_ROOT)
