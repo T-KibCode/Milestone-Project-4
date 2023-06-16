ee43482487c2074c749b813003cab6d0289684d4
T-KibCode <tkibirige94@gmail.com>
2023-06-16 09:57:09 +0000

    project migrations

--- milestonepp4/urls.py
+++ milestonepp4/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.url.statis import static
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls'))
-] + static(settings.MEDUA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
