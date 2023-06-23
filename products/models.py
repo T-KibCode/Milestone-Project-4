9bb985360541fc3a2c990d377534ba5c47896293
T-KibCode <tkibirige94@gmail.com>
2023-06-23 14:20:04 +0000

    added import_export feature

--- products/models.py
+++ products/models.py
@@ -13,27 +13,14 @@ class Category(models.Model):
         return self.friendly_name
 
 class Product(models.Model):
-    id = models.CharField(primary_key=True, max_length=10)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    type = models.CharField(max_length=254)
-    desc = models.TextField()
-    atk = models.CharField(max_length=5)
-    level = models.CharField(max_length=14)
-    race = models.CharField(max_length=254)
-    attribute = models.CharField(max_length=254)
-    scale = models.CharField(max_length=50)
-    archetype = models.CharField(max_length=100)
-    linkval = models.CharField(max_length=14)
-    linkmarkers = models.CharField(max_length=14)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image_url_small = models.URLField(max_length=1024, null=True, blank=True)
+    description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    formats = models.CharField(max_length=254)
-    has_effect = models.CharField(max_length=354)
-
-   
 
     def __str__(self):
         return self.name
