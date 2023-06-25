b1310e4821477ef6b7e704681e5d0d2c5af8fc67
T-KibCode <tkibirige94@gmail.com>
2023-06-25 14:22:20 +0000

    changed project to music portfolio website

--- Yugioh.session.sql
+++ Yugioh.session.sql
@@ -1,2 +1,64 @@
 --@block INSERT INTO 
-INSERT INTO cards(id,name,type,desc,atk,def,level,race,attribute,scale,archetype,linkval,linkmarkers,image_url,image_url_small,ban_tcg,ban_ocg,ban_goat,staple,views,viewsweek,upvotes,downvotes,formats,treated_as,tcg_date,ocg_date,konami_id,has_effect) VALUES( 34541863,'A Cell Breeding Device','Spell Card','During each of your Standby Phases, put 1 A-Counter on 1 face-up monster your opponent controls.','','','','Continuous','','','Alien','','','https://images.ygoprodeck.com/images/cards/34541863.jpg','https://images.ygoprodeck.com/images/cards_small/34541863.jpg','','','','',285331,0,118,110,'Duel Links|Common Charity|Edison|TCG|OCG','','2007-05-16','2007-02-15',7128.0,1);
+DROP TABLE cards
+
+--@block 
+
+-- Create Users table
+CREATE TABLE Users (
+    User_ID INT PRIMARY KEY,
+    Username VARCHAR(255),
+    Email VARCHAR(255),
+    Password VARCHAR(255)
+);
+
+-- Create Services table
+CREATE TABLE Services (
+    Service_ID INT PRIMARY KEY,
+    Service_Name VARCHAR(255),
+    Service_Description VARCHAR(255),
+    Service_Price DECIMAL(10,2)
+);
+
+-- Create Orders table
+CREATE TABLE Orders (
+    Order_ID INT PRIMARY KEY,
+    User_ID INT,
+    Service_ID INT,
+    Order_Date DATE,
+    -- Add other order-related fields here
+    FOREIGN KEY (User_ID) REFERENCES Users(User_ID),
+    FOREIGN KEY (Service_ID) REFERENCES Services(Service_ID)
+);
+
+-- Create Songs table
+CREATE TABLE Songs (
+    Song_ID INT PRIMARY KEY,
+    User_ID INT,
+    Song_Title VARCHAR(255),
+    Song_File_Path VARCHAR(255),
+    -- Add other song-related fields here
+    FOREIGN KEY (User_ID) REFERENCES Users(User_ID)
+);
+
+-- Create Videos table
+CREATE TABLE Videos (
+    Video_ID INT PRIMARY KEY,
+    User_ID INT,
+    Video_Title VARCHAR(255),
+    Video_File_Path VARCHAR(255),
+    -- Add other video-related fields here
+    FOREIGN KEY (User_ID) REFERENCES Users(User_ID)
+);
+
+-- Create Discounts table
+CREATE TABLE Discounts (
+    Discount_ID INT PRIMARY KEY,
+    Discount_Code VARCHAR(255),
+    Discount_Amount DECIMAL(10,2),
+    Minimum_Order_Amount DECIMAL(10,2)
+);
+
+-- Add Discount_ID column in Orders table
+ALTER TABLE Orders
+ADD Discount_ID INT,
+ADD FOREIGN KEY (Discount_ID) REFERENCES Discounts(Discount_ID);
