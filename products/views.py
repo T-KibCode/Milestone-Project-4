6d881f47af607da68bbe64b45cfefbb137eaf7c2
T-KibCode <tkibirige94@gmail.com>
2023-06-27 07:49:38 +0000

    product views/urls queries

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_products(request):
+    """ a view to show all products, including sorting and searching queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products' : products,
+    }
+
+    return render(request, 'products/products.html', context)
