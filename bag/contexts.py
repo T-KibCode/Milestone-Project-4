774458f003f703fb42f2a3395bbebc73227b7ba0
T-KibCode <tkibirige94@gmail.com>
2023-06-27 20:38:04 +0000

    amended some bag total options

---
+++ bag/contexts.py
@@ -0,0 +1,19 @@
+from decimal import Decimal
+from django.conf import settings
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
+
+    grand_total = total 
+
+    context = {
+        'bag_items' : bag_items,
+        'total' : total,
+        'product_count': product_count,
+        'grand_total': grand_total,
+    }
+
+    return context
