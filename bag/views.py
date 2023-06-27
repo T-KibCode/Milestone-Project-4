beeee42bd213e0b7316b89561dce1cb4efc719fd
T-KibCode <tkibirige94@gmail.com>
2023-06-27 21:37:10 +0000

    fixed my naming of product_detail.html file which was causing issues rendering products in bag selection pages

--- bag/views.py
+++ bag/views.py
@@ -6,3 +6,10 @@ def view_bag(request):
     """ A view that renders the bag content page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ A a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
