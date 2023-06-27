241e596257ec54d73198f26e428c415998daf9f1
T-KibCode <tkibirige94@gmail.com>
2023-06-27 07:19:58 +0000

    customermised admin

--- products/models.py
+++ products/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Category(models.Model):
+    class Meta: 
+        verbose_name_plural = "Catergories"
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
