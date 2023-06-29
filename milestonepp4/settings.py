27f23688cfa3292bfd54b52e7f2b6b48866dae39
T-KibCode <tkibirige94@gmail.com>
2023-06-29 12:59:25 +0000

    shipping bag

--- milestonepp4/settings.py
+++ milestonepp4/settings.py
@@ -48,6 +48,10 @@ INSTALLED_APPS = [
     'bag',
     'djmoney.contrib.exchange',
     'checkout',
+    
+
+    # other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -61,6 +65,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'milestonepp4.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -76,8 +82,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents', 
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
