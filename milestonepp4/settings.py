bafa38d82d3b2791ba767d2bfda33ed0f5f6fc28
T-KibCode <tkibirige94@gmail.com>
2023-07-06 23:52:27 +0100

    made new secret key, linked github file herokuapp

--- milestonepp4/settings.py
+++ milestonepp4/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6!kq8g1p7d90ay(qffxw_&mqyw%glju&e03#v=pfgbdj94sig&'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-tkibcode-milestoneproje-4jbakrd1py8.ws-eu101.gitpod.io', '127.0.0.1', 'acharlesmusic.herokuapp.com']
+ALLOWED_HOSTS = ['8000-tkibcode-milestoneproje-4jbakrd1py8.ws-eu101.gitpod.io', '127.0.0.1', 'acharlesmusic.herokuapp.com', 'localhost']
 
 
 # Application definition
