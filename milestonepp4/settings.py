4daf2545a579f895f5f9fd88a4a9f7714e6e4603
T-KibCode <tkibirige94@gmail.com>
2023-08-02 19:25:35 +0100

    changed the debug state of the static files to try and correct

--- milestonepp4/settings.py
+++ milestonepp4/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.conf import settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,13 +141,6 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
