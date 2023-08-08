3fd0ed4c82a15ef229db65610a25f1b0d590d5de
T-KibCode <tkibirige94@gmail.com>
2023-08-08 23:24:17 +0100

    added email responses

--- milestonepp4/settings.py
+++ milestonepp4/settings.py
@@ -223,4 +223,16 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'acharles-music@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
 django_heroku.settings(locals())
