3819f70faf0181773c4c6c9484922e63644eb0e6
T-KibCode <tkibirige94@gmail.com>
2023-08-08 22:20:12 +0100

    attempt at pushing with whitenoise settings middleware installed

--- milestonepp4/settings.py
+++ milestonepp4/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # whitenoise
 ]
 
 ROOT_URLCONF = 'milestonepp4.urls'
